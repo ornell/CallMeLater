5d24a2c2f727696e2a7b3e0e8e1574b5f4c437f5
Thomas Ornell <thor@abax.no>
2021-12-25 16:12:01 +0100

    Adding support for appsettings.json

--- go.mod
+++ go.mod
@@ -7,4 +7,10 @@ require (
 	github.com/lib/pq v1.10.4
 	github.com/mattn/go-sqlite3 v1.14.9
 	github.com/rs/zerolog v1.26.1
+	github.com/tkanos/gonfig v0.0.0-20210106201359-53e13348de2f
+)
+
+require (
+	github.com/ghodss/yaml v1.0.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	conf "github.com/tkanos/gonfig"
 	"os"
 )
 
@@ -12,15 +13,27 @@ var (
 	hasMore  = false
 )
 
+type Configuration struct {
+	dbType           string `json:"db_type" env:"DBTYPE"`
+	connectionString string `json:"connection_string" env:"CONSTRING"`
+}
+
 func main() {
+	C := Configuration{}
+	err := conf.GetConf("appsettings.json", &C)
+	if err != nil {
+		panic(err)
+	}
+
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
-	// PSQL
-	storage = newPgStorage("postgres://postgres:postgres@localhost:5432/callme?sslmode=disable")
-	// SQLite
-	// storage = newSqLiteStorage("file:storage.db?cache=shared&mode=memory")
+	if C.dbType == "PSQL" {
+		storage = newPgStorage(C.connectionString)
+	} else {
+		storage = newSqLiteStorage(C.connectionString)
+	}
 
 	go consumeLoop()
 	handleRequests()
